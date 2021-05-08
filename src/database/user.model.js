8ab9a1cd5774e169abdbd45326173cfafabdc1ae
azer <azerprojects@gmail.com>
2021-05-08 07:29:50 +0700

    fix: typo

--- src/database/user.model.js
+++ src/database/user.model.js
@@ -13,7 +13,7 @@ module.exports = function (mongoose) {
           type: Schema.Types.String,
           required: true,
           index: true,
-          unquie: true,
+          unique: true,
           select: true
         },
         email: {
