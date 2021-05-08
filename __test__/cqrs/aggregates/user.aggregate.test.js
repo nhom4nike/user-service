6c3dd5509db0e8889d1ebc77943e09ba1e610110
azer <azerprojects@gmail.com>
2021-05-08 08:06:56 +0700

    feat(test/aggregates): user tests

---
+++ __test__/cqrs/aggregates/user.aggregate.test.js
@@ -0,0 +1,37 @@
+const UserAggregate = require('@/cqrs/aggregates/user.aggregate')
+const UserRepository = require('@/cqrs/repositories/user.repository')
+
+const mockCreate = jest.fn()
+jest.mock('@/cqrs/repositories/user.repository', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      create: mockCreate
+    }
+  })
+})
+
+beforeEach(() => {
+  UserRepository.mockClear()
+  mockCreate.mockClear()
+})
+
+test('should constructor be call', () => {
+  const aggregate = new UserAggregate(new UserRepository())
+  expect(aggregate).toBeTruthy()
+  expect(UserRepository).toBeCalledTimes(1)
+})
+
+test('should create fucntion be called', async () => {
+  const aggregate = new UserAggregate(new UserRepository())
+  await aggregate.command('create', {
+    username: '1',
+    email: '1',
+    password: '1'
+  })
+  expect(mockCreate).toBeCalledTimes(1)
+  expect(mockCreate).toBeCalledWith({
+    username: '1',
+    email: '1',
+    password: '1'
+  })
+})
