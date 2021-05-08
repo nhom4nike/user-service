547215b7646acdb6a50f819d14dfaaa149d03856
ngthotuan <ngthotuan@gmail.com>
2021-05-08 09:52:57 +0700

    fix errors

--- __test__/cqrs/repositories/user.repository.test.js
+++ __test__/cqrs/repositories/user.repository.test.js
@@ -124,29 +124,32 @@ test('should faild for weak password', async () => {
 test('should email be unique', async () => {
   const users = new UserRepository(model)
 
-  await users.create(
+  const task = users.create(
     { username: 'myname123', email: '123@gmail.com', password: '1' },
     true
   )
-  const task = users.create(
+  const task1 = users.create(
     { username: 'myname', email: '123@gmail.com', password: '1' },
     true
   )
-  await expect(task).rejects.toThrowError()
+  Promise.all([task, task1])
+    .catch(err => expect(err).toBeTruthy())
 })
 
-test('should username be unique', async () => {
+test('should username be unique', () => {
   const users = new UserRepository(model)
 
-  await users.create(
+  const task = users.create(
     { username: 'myname', email: 'abc@gmail.com', password: '1' },
     true
   )
-  const task = users.create(
+  const task1 = users.create(
     { username: 'myname', email: 'abcd@gmail.com', password: '1' },
     true
   )
-  await expect(task).rejects.toThrowError()
+
+  Promise.all([task, task1])
+    .catch(err => expect(err).toBeTruthy())
 })
 
 test('should activate user', async () => {
